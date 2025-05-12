23f478173d65931e56be3ecb47409a1290c820b7
Simon Wang <wang.c.simon@gmail.com>
2025-05-12 00:54:46 -0400

    add new experiments and fix baseline scriipts

---
+++ simon-testing/shell-scripts/extreme_contention_batch_time.sh
@@ -0,0 +1,10 @@
+./bench_ycsb --logtostderr=1 --id=0 --servers="127.0.0.1:10000" --protocol=Aria --partition_num=4 --threads=4 --batch_size=1000 --read_write_ratio=80 --cross_ratio=90 --keys=18000 --zipf=0.99 --batch_time_ms=5
+./bench_ycsb --logtostderr=1 --id=0 --servers="127.0.0.1:10001" --protocol=Aria --partition_num=4 --threads=4 --batch_size=1000 --read_write_ratio=80 --cross_ratio=90 --keys=18000 --zipf=0.99 --batch_time_ms=5
+./bench_ycsb --logtostderr=1 --id=0 --servers="127.0.0.1:10002" --protocol=Aria --partition_num=4 --threads=4 --batch_size=1000 --read_write_ratio=80 --cross_ratio=90 --keys=18000 --zipf=0.99 --batch_time_ms=5
+./bench_ycsb --logtostderr=1 --id=0 --servers="127.0.0.1:10003" --protocol=Aria --partition_num=4 --threads=4 --batch_size=1000 --read_write_ratio=80 --cross_ratio=90 --keys=18000 --zipf=0.99 --batch_time_ms=5
+./bench_ycsb --logtostderr=1 --id=0 --servers="127.0.0.1:10004" --protocol=Aria --partition_num=4 --threads=4 --batch_size=1000 --read_write_ratio=80 --cross_ratio=90 --keys=18000 --zipf=0.99 --batch_time_ms=5
+./bench_ycsb --logtostderr=1 --id=0 --servers="127.0.0.1:10000" --protocol=Aria --partition_num=4 --threads=4 --batch_size=1000 --read_write_ratio=80 --cross_ratio=90 --keys=18000 --zipf=0.99 --batch_time_ms=5
+./bench_ycsb --logtostderr=1 --id=0 --servers="127.0.0.1:10001" --protocol=Aria --partition_num=4 --threads=4 --batch_size=1000 --read_write_ratio=80 --cross_ratio=90 --keys=18000 --zipf=0.99 --batch_time_ms=5
+./bench_ycsb --logtostderr=1 --id=0 --servers="127.0.0.1:10002" --protocol=Aria --partition_num=4 --threads=4 --batch_size=1000 --read_write_ratio=80 --cross_ratio=90 --keys=18000 --zipf=0.99 --batch_time_ms=5
+./bench_ycsb --logtostderr=1 --id=0 --servers="127.0.0.1:10003" --protocol=Aria --partition_num=4 --threads=4 --batch_size=1000 --read_write_ratio=80 --cross_ratio=90 --keys=18000 --zipf=0.99 --batch_time_ms=5
+./bench_ycsb --logtostderr=1 --id=0 --servers="127.0.0.1:10004" --protocol=Aria --partition_num=4 --threads=4 --batch_size=1000 --read_write_ratio=80 --cross_ratio=90 --keys=18000 --zipf=0.99 --batch_time_ms=5
